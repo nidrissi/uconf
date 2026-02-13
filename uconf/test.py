8ebcff8bd19175ec10c48dab3f8827ce0da78c23
Najib Idrissi <najib.idrissi-kaitouni@u-pariscite.fr>
2026-02-13 17:18:22 +0100

    I hate python :-(

--- uconf/test.py
+++ uconf/test.py
@@ -1,12 +1,5 @@
-from uconf import *
+from hello import *
 
-SE2 = Surjection(2)
-u = SE2([1, 2])
-v = SE2([1, 2])
-Surjection.compose(u, 1, v)
-
-BE2 = BarrattEccles(2)
-x = BE2(([1, 2], [2, 1], [1, 2]))
+BBE3 = BarConstruction(BarrattEccles, 3)
+x = BBE3(LabelledOrderedTree([], 42))
 print(x)
-u = x.table_reduction()
-print(u)
