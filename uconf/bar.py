f2e307590120763762fc928b2e5ab2528392a667
Najib Idrissi-Kaïtouni <najib.idrissi.kaitouni@outlook.com>
2026-02-13 17:18:22 +0100

    I hate python :-(

--- uconf/bar.py
+++ uconf/bar.py
@@ -1,23 +1,49 @@
 from operad import *
 from sage.all import *  # pyright: ignore[reportWildcardImportFromLibrary]
-from sage.combinat.rooted_tree import LabelledRootedTree
 
 
 class BarConstruction(CombinatorialFreeModule):
     def __init__(self, operad: type[OperadProtocol], n: int, base_ring=QQ):
-        indices = Set(LabelledRootedTree)
         super().__init__(
             base_ring,
-            indices,
+            LabelledOrderedTrees(),
             prefix=f"B({operad.name}){n}",
             category=GradedModulesWithBasis(base_ring),
         )
-        self.operad = operad
+        self._operad = operad
         self.name = f"B({operad.name}){n}"
-        self.arity = n
+        self._arity = n
+        self.boundary = self.module_morphism(
+            on_basis=self._boundary_on_basis, codomain=self
+        )
 
-    def degree_on_basis(self, tree):
+    def _element_constructor_(self, x):
+        return super()._element_constructor_(x)
+
+    def arity(self) -> int:
+        return self._arity
+
+    def degree_on_basis(self, tree: LabelledOrderedTree) -> int:
         """
         Degree = Sum(|p| + 1 for p in labels)
         """
-        return sum(label.degree() + 1 for label in tree.labels())
+
+        def term_generator():
+            for label in tree.labels():
+                if label is None:
+                    raise ValueError("Labels of the tree cannot be None.")
+                yield label.degree() + 1
+
+        return sum(term_generator())
+
+    def _boundary_on_basis(
+        self, tree: LabelledOrderedTree
+    ) -> "BarConstruction.Element":
+        raise NotImplementedError("Boundary computation is not implemented yet.")
+
+    class Element(CombinatorialFreeModule.Element):
+        def arity(self) -> int:
+            return self.parent().arity()
+
+        def boundary(self) -> "BarConstruction.Element":
+            return self.parent().boundary(self)
