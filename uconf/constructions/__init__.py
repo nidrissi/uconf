e5ed0e337cb8c558bad17a40cf6ada1479dc5af0
Najib Idrissi <najib.idrissi-kaitouni@u-pariscite.fr>
2026-03-13 15:17:12 +0100

    rename comodule.py => e_comodule.py

--- uconf/constructions/__init__.py
+++ uconf/constructions/__init__.py
@@ -4,7 +4,7 @@ from uconf.constructions.bar_algebra import BarComplexAlgebra
 from uconf.constructions.bar_construction import BarConstruction
 from uconf.constructions.cobar_construction import CobarConstruction
 from uconf.constructions.cobar_coalgebra import CobarComplexCoalgebra
-from uconf.constructions.comodule import e_comodule_on_generator
+from uconf.constructions.e_comodule import e_comodule_on_generator
 
 __all__ = [
     "BarConstruction",
