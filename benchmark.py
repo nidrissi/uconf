09f816557c2dca92f37c18032d7b373026fc7a43
victor roca lucio <victor.r.lucio@hotmail.com>
2026-03-26 13:23:55 +0100

    Added finite fields

--- benchmark.py
+++ benchmark.py
@@ -1,5 +1,16 @@
 from uconf import euclidean_unordered_configuration_model, compute_chain_complex
 from sage.all import QQ
 
-model = euclidean_unordered_configuration_model(QQ, 2)
-cc = compute_chain_complex(model, degrees=range(-2, 4), weight=3)
+from sage.all import *
+# or more specifically:
+from sage.rings.finite_rings.finite_field_constructor import GF
+
+#Compute the chain complex over any field:
+
+F = GF(2)
+
+model = euclidean_unordered_configuration_model(F, 2)
+cc = compute_chain_complex(model, degrees=range(-2, 4), weight = 3)
+
+
+
